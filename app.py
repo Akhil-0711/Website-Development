8b5f313b4bbf748ee5793302c09794bd4ef969ed
Akhil-0711 <92972127+Akhil-0711@users.noreply.github.com>
2024-01-06 11:26:55 +0000

    Added Content list

--- app.py
+++ app.py
@@ -2,9 +2,31 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+BREADS = [
+  {
+    'id': 1,
+    'name': 'Beagle',
+    'description':'3yrs old, playful,friendly',
+    'location': 'New Delhi'
+  },
+  {
+    'id': 2,
+    'name': 'Splitz',
+    'description':'2yrs old, cute,friendly',
+    'location': 'New Delhi'
+  },
+  {
+    'id': 3,
+    'name': 'Labra',
+    'description':'3yrs old, playful,friendly',
+    'location': 'New Delhi'
+  }
+]
+
 @app.route("/")
 def hello_world():
-    return render_template('home.html')
+    return render_template('home.html',
+                           breads = BREADS)
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
